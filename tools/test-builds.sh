b7c478be084165a110e9f1ef75e1b0417a799c2a
Dennis Hamester <dennis.hamester@startmail.com>
2017-03-25 11:46:15 +0100

    tools: Add test-builds.sh

---
+++ tools/test-builds.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+SCRIPT=`realpath "${0}"`
+SCRIPTPATH=`dirname "${SCRIPT}"`
+PROJECTROOT=`realpath "${SCRIPTPATH}"/..`
+
+function test_build() {
+    echo "Building feature $1"
+    cargo build -v --no-default-features --features $1 || exit 1
+    echo ""
+}
+
+cd "${PROJECTROOT}"
+
+test_build vk_1_0_3
+test_build vk_1_0_4
+test_build vk_1_0_5
+test_build vk_1_0_6
+test_build vk_1_0_7
