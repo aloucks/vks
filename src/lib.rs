be60b8436f0f00a511e9ef0f86e6448097a973f3
Dennis Hamester <dennis.hamester@startmail.com>
2017-06-25 09:13:17 +0200

    lib: Remove feature struct_field_attributes attribute
    It is no longer required with the current nightly compiler.

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
-#![cfg_attr(feature = "unstable_rust", feature(struct_field_attributes))]
 #![cfg_attr(feature = "unstable_rust", feature(untagged_unions))]
 
 extern crate libc;
